80d66b51582e89f8363c848d3de8ba941ad709fc
Alexander Lelidis <alelidis@esri.com>
2020-03-26 17:41:08 +0100

    work on torch open pose

--- torch_openpose/body.py
+++ torch_openpose/body.py
@@ -29,72 +29,77 @@ class Body(object):
         self.model.eval()
         self.stride = 8
         self.padValue = 128
+        self.pad = None
+        self.imageToTest_padded_shape = None
 
     def compute_heatmap(self, input_tensor):
         _, heat_tensor = self.model(input_tensor)
         heat_tensor_np = heat_tensor.cpu().detach().numpy()
 
-        heatmap = np.transpose(np.squeeze(heat_tensor_np), (1, 2, 0))  # output 1 is heatmaps
+        heatmap = np.transpose(np.squeeze(heat_tensor_np),
+                               (1, 2, 0))  # output 1 is heatmaps
         return heatmap, heat_tensor
 
     def compute_paf(self, input_tensor):
         paf_tensor, _ = self.model(input_tensor)
         paf_tensor_np = paf_tensor.cpu().detach().numpy()
 
-        paf = np.transpose(np.squeeze(paf_tensor_np), (1, 2, 0))  # output 1 is heatmaps
+        paf = np.transpose(np.squeeze(paf_tensor_np),
+                           (1, 2, 0))  # output 1 is heatmaps
         return paf, paf_tensor
 
-    def image_to_input_tensor(self, imageToTest):
-        imageToTest_padded, pad = util.padRightDownCorner(imageToTest, self.stride, self.padValue)
-        im = np.transpose(np.float32(imageToTest_padded[:, :, :, np.newaxis]), (3, 2, 0, 1)) / 256 - 0.5
+    def compute_heatmap_paf_avg(self, data, oriImg_shape):
+        heatmap_avg = np.zeros((oriImg_shape[0], oriImg_shape[1], 19))
+        paf_avg = np.zeros((oriImg_shape[0], oriImg_shape[1], 38))
+
+        with torch.no_grad():
+            Mconv7_stage6_L1, Mconv7_stage6_L2 = self.model(data)
+        Mconv7_stage6_L1 = Mconv7_stage6_L1.cpu().numpy()
+        Mconv7_stage6_L2 = Mconv7_stage6_L2.cpu().numpy()
+        # extract outputs, resize, and remove padding
+        # heatmap = np.transpose(np.squeeze(net.blobs[output_blobs.keys()[1]].data), (1, 2, 0))  # output 1 is heatmaps
+        # output 1 is heatmaps
+        heatmap = np.transpose(np.squeeze(Mconv7_stage6_L2), (1, 2, 0))
+        heatmap = cv2.resize(
+            heatmap, (0, 0), fx=self.stride, fy=self.stride, interpolation=cv2.INTER_CUBIC)
+        heatmap = heatmap[:self.imageToTest_padded_shape[0] -
+                          self.pad[2], :self.imageToTest_padded_shape[1] - self.pad[3], :]
+        heatmap = cv2.resize(
+            heatmap, (oriImg_shape[1], oriImg_shape[0]), interpolation=cv2.INTER_CUBIC)
+
+        # paf = np.transpose(np.squeeze(net.blobs[output_blobs.keys()[0]].data), (1, 2, 0))  # output 0 is PAFs
+        paf = np.transpose(np.squeeze(Mconv7_stage6_L1),
+                           (1, 2, 0))  # output 0 is PAFs
+        paf = cv2.resize(paf, (0, 0), fx=self.stride,
+                         fy=self.stride, interpolation=cv2.INTER_CUBIC)
+        paf = paf[:self.imageToTest_padded_shape[0] - self.pad[2],
+                  :self.imageToTest_padded_shape[1] - self.pad[3], :]
+        paf = cv2.resize(
+            paf, (oriImg_shape[1], oriImg_shape[0]), interpolation=cv2.INTER_CUBIC)
+
+        heatmap_avg += heatmap_avg + heatmap
+        paf_avg += + paf
+        return heatmap_avg, paf_avg
+
+    def image_to_input_tensor(self, imageToTest, set_local_state=False):
+        imageToTest_padded, pad = util.padRightDownCorner(
+            imageToTest, self.stride, self.padValue)
+        im = np.transpose(np.float32(
+            imageToTest_padded[:, :, :, np.newaxis]), (3, 2, 0, 1)) / 256 - 0.5
         im = np.ascontiguousarray(im)
+
+        if set_local_state:
+            self.imageToTest_padded_shape = imageToTest_padded.shape
+            self.pad = pad
 
         return torch.from_numpy(im).float()
 
-    def __call__(self, oriImg):
-        # scale_search = [0.5, 1.0, 1.5, 2.0]
-        scale_search = [1.0]
-        boxsize = 368
-        thre1 = 0.1
-        thre2 = 0.05
-        multiplier = [x * boxsize / oriImg.shape[0] for x in scale_search]
-        heatmap_avg = np.zeros((oriImg.shape[0], oriImg.shape[1], 19))
-        paf_avg = np.zeros((oriImg.shape[0], oriImg.shape[1], 38))
-
-        for m in range(len(multiplier)):
-            scale = multiplier[m]
-            imageToTest = cv2.resize(oriImg, (0, 0), fx=scale, fy=scale, interpolation=cv2.INTER_CUBIC)
-            imageToTest_padded, pad = util.padRightDownCorner(imageToTest, self.stride, self.padValue)
-            im = np.transpose(np.float32(imageToTest_padded[:, :, :, np.newaxis]), (3, 2, 0, 1)) / 256 - 0.5
-            im = np.ascontiguousarray(im)
-
-            data = torch.from_numpy(im).float()
-            if torch.cuda.is_available() and self.useGPU:
-                data = data.cuda()
-            # data = data.permute([2, 0, 1]).unsqueeze(0).float()
-            with torch.no_grad():
-                Mconv7_stage6_L1, Mconv7_stage6_L2 = self.model(data)
-            Mconv7_stage6_L1 = Mconv7_stage6_L1.cpu().numpy()
-            Mconv7_stage6_L2 = Mconv7_stage6_L2.cpu().numpy()
-
-            # extract outputs, resize, and remove padding
-            # heatmap = np.transpose(np.squeeze(net.blobs[output_blobs.keys()[1]].data), (1, 2, 0))  # output 1 is heatmaps
-            heatmap = np.transpose(np.squeeze(Mconv7_stage6_L2), (1, 2, 0))  # output 1 is heatmaps
-            heatmap = cv2.resize(heatmap, (0, 0), fx=self.stride, fy=self.stride, interpolation=cv2.INTER_CUBIC)
-            heatmap = heatmap[:imageToTest_padded.shape[0] - pad[2], :imageToTest_padded.shape[1] - pad[3], :]
-            heatmap = cv2.resize(heatmap, (oriImg.shape[1], oriImg.shape[0]), interpolation=cv2.INTER_CUBIC)
-
-            # paf = np.transpose(np.squeeze(net.blobs[output_blobs.keys()[0]].data), (1, 2, 0))  # output 0 is PAFs
-            paf = np.transpose(np.squeeze(Mconv7_stage6_L1), (1, 2, 0))  # output 0 is PAFs
-            paf = cv2.resize(paf, (0, 0), fx=self.stride, fy=self.stride, interpolation=cv2.INTER_CUBIC)
-            paf = paf[:imageToTest_padded.shape[0] - pad[2], :imageToTest_padded.shape[1] - pad[3], :]
-            paf = cv2.resize(paf, (oriImg.shape[1], oriImg.shape[0]), interpolation=cv2.INTER_CUBIC)
-
-            heatmap_avg += heatmap_avg + heatmap / len(multiplier)
-            paf_avg += + paf / len(multiplier)
-
+    # TODO: replace with cpp version
+    def get_pose(self, heatmap_avg, paf_avg, oriImg_shape):
         all_peaks = []
         peak_counter = 0
+        thre1 = 0.1
+        thre2 = 0.05
 
         for part in range(18):
             map_ori = heatmap_avg[:, :, part]
@@ -111,21 +116,25 @@ class Body(object):
 
             peaks_binary = np.logical_and.reduce(
                 (one_heatmap >= map_left, one_heatmap >= map_right, one_heatmap >= map_up, one_heatmap >= map_down, one_heatmap > thre1))
-            peaks = list(zip(np.nonzero(peaks_binary)[1], np.nonzero(peaks_binary)[0]))  # note reverse
+            peaks = list(zip(np.nonzero(peaks_binary)[1], np.nonzero(
+                peaks_binary)[0]))  # note reverse
             peaks_with_score = [x + (map_ori[x[1], x[0]],) for x in peaks]
             peak_id = range(peak_counter, peak_counter + len(peaks))
-            peaks_with_score_and_id = [peaks_with_score[i] + (peak_id[i],) for i in range(len(peak_id))]
+            peaks_with_score_and_id = [
+                peaks_with_score[i] + (peak_id[i],) for i in range(len(peak_id))]
 
             all_peaks.append(peaks_with_score_and_id)
             peak_counter += len(peaks)
 
         # find connection in the specified sequence, center 29 is in the position 15
-        limbSeq = [[2, 3], [2, 6], [3, 4], [4, 5], [6, 7], [7, 8], [2, 9], [9, 10], \
-                   [10, 11], [2, 12], [12, 13], [13, 14], [2, 1], [1, 15], [15, 17], \
+        limbSeq = [[2, 3], [2, 6], [3, 4], [4, 5], [6, 7], [7, 8], [2, 9], [9, 10],
+                   [10, 11], [2, 12], [12, 13], [
+                       13, 14], [2, 1], [1, 15], [15, 17],
                    [1, 16], [16, 18], [3, 17], [6, 18]]
-        # the middle joints heatmap correpondence
-        mapIdx = [[31, 32], [39, 40], [33, 34], [35, 36], [41, 42], [43, 44], [19, 20], [21, 22], \
-                  [23, 24], [25, 26], [27, 28], [29, 30], [47, 48], [49, 50], [53, 54], [51, 52], \
+        # the middle joints heatmap correspondence
+        mapIdx = [[31, 32], [39, 40], [33, 34], [35, 36], [41, 42], [43, 44], [19, 20], [21, 22],
+                  [23, 24], [25, 26], [27, 28], [29, 30], [
+                      47, 48], [49, 50], [53, 54], [51, 52],
                   [55, 56], [37, 38], [45, 46]]
 
         connection_all = []
@@ -147,29 +156,33 @@ class Body(object):
                         norm = math.sqrt(vec[0] * vec[0] + vec[1] * vec[1])
                         vec = np.divide(vec, norm)
 
-                        startend = list(zip(np.linspace(candA[i][0], candB[j][0], num=mid_num), \
+                        startend = list(zip(np.linspace(candA[i][0], candB[j][0], num=mid_num),
                                             np.linspace(candA[i][1], candB[j][1], num=mid_num)))
 
-                        vec_x = np.array([score_mid[int(round(startend[I][1])), int(round(startend[I][0])), 0] \
+                        vec_x = np.array([score_mid[int(round(startend[I][1])), int(round(startend[I][0])), 0]
                                           for I in range(len(startend))])
-                        vec_y = np.array([score_mid[int(round(startend[I][1])), int(round(startend[I][0])), 1] \
+                        vec_y = np.array([score_mid[int(round(startend[I][1])), int(round(startend[I][0])), 1]
                                           for I in range(len(startend))])
 
-                        score_midpts = np.multiply(vec_x, vec[0]) + np.multiply(vec_y, vec[1])
+                        score_midpts = np.multiply(
+                            vec_x, vec[0]) + np.multiply(vec_y, vec[1])
                         score_with_dist_prior = sum(score_midpts) / len(score_midpts) + min(
-                            0.5 * oriImg.shape[0] / norm - 1, 0)
-                        criterion1 = len(np.nonzero(score_midpts > thre2)[0]) > 0.8 * len(score_midpts)
+                            0.5 * oriImg_shape[0] / norm - 1, 0)
+                        criterion1 = len(np.nonzero(score_midpts > thre2)[
+                                         0]) > 0.8 * len(score_midpts)
                         criterion2 = score_with_dist_prior > 0
                         if criterion1 and criterion2:
                             connection_candidate.append(
                                 [i, j, score_with_dist_prior, score_with_dist_prior + candA[i][2] + candB[j][2]])
 
-                connection_candidate = sorted(connection_candidate, key=lambda x: x[2], reverse=True)
+                connection_candidate = sorted(
+                    connection_candidate, key=lambda x: x[2], reverse=True)
                 connection = np.zeros((0, 5))
                 for c in range(len(connection_candidate)):
                     i, j, s = connection_candidate[c][0:3]
                     if (i not in connection[:, 3] and j not in connection[:, 4]):
-                        connection = np.vstack([connection, [candA[i][3], candB[j][3], s, i, j]])
+                        connection = np.vstack(
+                            [connection, [candA[i][3], candB[j][3], s, i, j]])
                         if (len(connection) >= min(nA, nB)):
                             break
 
@@ -181,7 +194,8 @@ class Body(object):
         # last number in each row is the total parts number of that person
         # the second last number in each row is the score of the overall configuration
         subset = -1 * np.ones((0, 20))
-        candidate = np.array([item for sublist in all_peaks for item in sublist])
+        candidate = np.array(
+            [item for sublist in all_peaks for item in sublist])
 
         for k in range(len(mapIdx)):
             if k not in special_k:
@@ -202,10 +216,12 @@ class Body(object):
                         if subset[j][indexB] != partBs[i]:
                             subset[j][indexB] = partBs[i]
                             subset[j][-1] += 1
-                            subset[j][-2] += candidate[partBs[i].astype(int), 2] + connection_all[k][i][2]
+                            subset[j][-2] += candidate[partBs[i].astype(
+                                int), 2] + connection_all[k][i][2]
                     elif found == 2:  # if found 2 and disjoint, merge them
                         j1, j2 = subset_idx
-                        membership = ((subset[j1] >= 0).astype(int) + (subset[j2] >= 0).astype(int))[:-2]
+                        membership = ((subset[j1] >= 0).astype(
+                            int) + (subset[j2] >= 0).astype(int))[:-2]
                         if len(np.nonzero(membership == 2)[0]) == 0:  # merge
                             subset[j1][:-2] += (subset[j2][:-2] + 1)
                             subset[j1][-2:] += subset[j2][-2:]
@@ -214,7 +230,8 @@ class Body(object):
                         else:  # as like found == 1
                             subset[j1][indexB] = partBs[i]
                             subset[j1][-1] += 1
-                            subset[j1][-2] += candidate[partBs[i].astype(int), 2] + connection_all[k][i][2]
+                            subset[j1][-2] += candidate[partBs[i].astype(
+                                int), 2] + connection_all[k][i][2]
 
                     # if find no partA in the subset, create a new subset
                     elif not found and k < 17:
@@ -222,7 +239,8 @@ class Body(object):
                         row[indexA] = partAs[i]
                         row[indexB] = partBs[i]
                         row[-1] = 2
-                        row[-2] = sum(candidate[connection_all[k][i, :2].astype(int), 2]) + connection_all[k][i][2]
+                        row[-2] = sum(candidate[connection_all[k][i,
+                                                                  :2].astype(int), 2]) + connection_all[k][i][2]
                         subset = np.vstack([subset, row])
         # delete some rows of subset which has few parts occur
         deleteIdx = []
@@ -234,6 +252,16 @@ class Body(object):
         # subset: n*20 array, 0-17 is the index in candidate, 18 is the total score, 19 is the total parts
         # candidate: x, y, score, id
         return candidate, subset
+
+    def __call__(self, oriImg):
+        data = self.image_to_input_tensor(oriImg, True)
+
+        if torch.cuda.is_available() and self.useGPU:
+            data = data.cuda()
+
+        heatmap_avg, paf_avg = self.compute_heatmap_paf_avg(data, oriImg.shape)
+        return self.get_pose(heatmap_avg, paf_avg, oriImg.shape)
+
 
 if __name__ == "__main__":
     body_estimation = Body('../model/body_pose_model.pth')
